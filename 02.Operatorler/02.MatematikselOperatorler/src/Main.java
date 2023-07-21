e6f4ed8951f750970e4a1ec6fc45ad7b3fe65dde
Burak Zafer Özcan <ozcanburakzafer@gmail.com>
2023-07-21 11:17:27 +0300

    Add files via upload

---
+++ 02.Operatorler/02.MatematikselOperatorler/src/Main.java
@@ -0,0 +1,45 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// matematiksel operatörler "+","-","/","*","%","++","--","+=","-=","/=","*="
+
+		int topla = 5 + 3;
+		int cikar = 5 - 3;
+		int carp = 5 * 5;
+		int bol = 6 / 2;
+		int modAl = 10 % 2;
+		System.out.println(topla);
+		System.out.println(cikar);
+		System.out.println(carp);
+		System.out.println(bol);
+		System.out.println(modAl);
+		System.out.println("---------------------------------");
+		int arttir = 5;
+		arttir++;
+		int eksilt = 5;
+		eksilt--;
+		System.out.println(arttir);
+		System.out.println(eksilt);
+		System.out.println("---------------------------------");
+		int sayi1 = 5;
+		int sayi2 = 3;
+		int sonuc1 = 5;
+		int sonuc2 = 5;
+		int sonuc3 = 5;
+		int sonuc4 = 5;
+		int sonuc5 = 5;
+		sonuc1 += sayi1;
+		sonuc2 -= sayi2;
+		sonuc3 *= sayi1;
+		sonuc4 /= sayi2;
+		sonuc5 %= sayi1;
+		System.out.println(sonuc1);
+		System.out.println(sonuc2);
+		System.out.println(sonuc3);
+		System.out.println(sonuc4);
+		System.out.println(sonuc5);
+
+	}
+
+}
